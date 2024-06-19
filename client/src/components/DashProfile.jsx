cc46ecb14e8b4dc6de51637908d8be10cf04eecb
sahchandan781 <sahchandan781@gmail.com>
2024-06-19 13:36:10 +0530

    complete delete user functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,4 +1,4 @@
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import {
@@ -10,11 +10,12 @@ import {
 import { app } from "../firebase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import { updateStart, updateSuccess, UpdateFailure } from "../redux/user/userSlice";
+import { updateStart, updateSuccess, UpdateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from "../redux/user/userSlice";
 import { useDispatch } from "react-redux";
+import { HiOutlineExclamationCircle } from 'react-icons/hi'
 
 export default function DashProfile() {
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, error } = useSelector((state) => state.user);
   const [imageFile, setImageFile] = useState(null);
   const [imageFileUrl, setImageFileUrl] = useState(null);
   const filePickerRef = useRef();
@@ -25,6 +26,7 @@ export default function DashProfile() {
   const [imageFileUploading, setImageFileUploading ] = useState(false);
   const [updateUserSuccess, setUpdateUserSuccess ] = useState(null);
   const [updateUserError, setUpdateUserError ] = useState(null);
+  const [showModel, setShowModel] = useState(false);
   const [formData, setFormData] = useState({});
 
   const handleImageChange = (e) => {
@@ -112,6 +114,23 @@ export default function DashProfile() {
       setUpdateUserError(error.message);
     }
   }
+  const handleDeleteUser = async () =>{
+    setShowModel(false);
+    try {
+      dispatch(deleteUserStart);
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+       const data = await res.json();
+       if(!res.ok){
+        dispatch(deleteUserFailure(data.message));
+       }else{
+        dispatch(deleteUserSuccess(data));
+       }
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
 
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
@@ -175,12 +194,17 @@ export default function DashProfile() {
         </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
-        <span>Delete Account</span>
-        <span>Sign Out</span>
+        <span onClick={() =>setShowModel(true)} className="cursor-pointer">Delete Account</span>
+        <span className="cursor-pointer">Sign Out</span>
       </div>
       {updateUserSuccess && ( 
         <Alert color='success' className="mt-5">
           {updateUserSuccess}
+        </Alert>
+      )}
+      {error && ( 
+        <Alert color='failure' className="mt-5">
+          {error}
         </Alert>
       )}
       {updateUserError && ( 
@@ -188,6 +212,19 @@ export default function DashProfile() {
           {updateUserError}
         </Alert>
       )}
+      <Modal show={showModel} onClose={() => setShowModel(false)} popup size='md'>
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto"/>
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete your account?</h3>
+            <div className="flex justify-center gap-4">
+              <Button color='failure' onClick={handleDeleteUser}>Yes, I'm sure</Button>
+              <Button color='gray' onClick={()=> setShowModel(false)}>No, cancel</Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,44 +1,67 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    currentUser: null,
-    error: null,
-    loading: false,
-}
+  currentUser: null,
+  error: null,
+  loading: false,
+};
 
 const userSlice = createSlice({
-    name: 'user',
-    initialState,
-    reducers:{
-        signInStart: (state) =>{
-            state.loading = true;
-            state.error = null;
-        },
-        signInSuccess: (state, action) =>{
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
-        },
-        signInFailure: (state, action) =>{
-            state.loading = false;
-            state.error = action.payload;
-        },
-        updateStart: (state) =>{
-            state.loading = true;
-            state.error = null;
-        },
-        updateSuccess: (state, action) =>{
-            state.currentUser = action.payload;
-            state.loading = false;
-            state.error = null;
-        },
-        UpdateFailure: (state, action) =>{
-            state.loading = false;
-            state.error = action.payload;
-        },
-    }
-})
+  name: "user",
+  initialState,
+  reducers: {
+    signInStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    signInSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    signInFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    UpdateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteUserSuccess: (state, action) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
 
-export const { signInStart, signInSuccess, signInFailure, updateStart, updateSuccess, UpdateFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  UpdateFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure
+} = userSlice.actions;
 
 export default userSlice.reducer;
