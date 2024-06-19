d13e9341959063caaa30ebfaf3c32264091cbf23
sahchandan781 <sahchandan781@gmail.com>
2024-06-19 12:13:45 +0530

    complete user update profile functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -32,6 +32,7 @@ export const updateUser = async (req, res, next) => {
           errorHandler(400, 'Username can only contain letters and numbers')
         );
       }
+    }
       try {
         const updatedUser = await User.findByIdAndUpdate(
           req.params.userId,
@@ -50,5 +51,5 @@ export const updateUser = async (req, res, next) => {
       } catch (error) {
         next(error);
       }
-    }
+    
   };
